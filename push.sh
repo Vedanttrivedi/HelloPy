d891950b0d14582ada2b1760fb98df60d130777a
Vedanttrivedi <vedtrive@gmail.com>
2024-04-24 22:09:41 +0530

    modified bash

--- push.sh
+++ push.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ids=$(az webapp list --query '[0].id' -o tsv)
+echo $ids
 name="HelloPy"
-repo_url="https://github.com/Vedanttrivedi/HelloPy"
+repo_url=https://github.com/Vedanttrivedi/HelloPy
+echo $repo_url
 az webapp deployment source config --branch main --manual-integration --name $name --repo-url $repo_url --ids $ids
 az webapp deployment source sync --ids $ids --name $name
 echo "pushing"
