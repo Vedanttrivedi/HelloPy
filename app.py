587d8306a0b8f36cda99b7b71f4734d0c54e5680
Vedanttrivedi <53808109+Vedanttrivedi@users.noreply.github.com>
2024-05-02 12:28:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'setting
 
 @app.route('/')
 def index():
-   #user_data = User.sendData()
+   user_data = User.sendData()
    return render_template('index.html',users={})
 
 @app.route('/favicon.ico')
