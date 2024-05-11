8d41b086c3de8fca5dffd1014c2c8f412474b2f9
Vedanttrivedi <53808109+Vedanttrivedi@users.noreply.github.com>
2024-05-11 21:10:06 +0530

    Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ids="/subscriptions/e1231fd8-853d-4f48-80b3-6fdc695638e7/resourcegroups/managed-identity-test/providers/Microsoft.Web/sites/python-app-identity"
-echo $ids
-repo_url=https://github.com/Vedanttrivedi/HelloPy
-echo $repo_url
-az webapp deployment source config --branch main --manual-integration  --repo-url $repo_url --ids $ids
-az webapp deployment source sync --ids $ids --name $name
-echo "pushing"
